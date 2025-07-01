1298c812b4ab721cd723e9d2a9719c75d95daef9
emaad-brainwonders <emaad.brainwonders@gmail.com>
2025-07-01 15:51:46 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { registerDateTool } from "./tools/date";
 import { setupAppointmentTools } from "./tools/appointment";
 import { registerEmailTools } from "./tools/mail";
 import { CalendarReminderService } from "./automation/calendarreminder";
+import { registerReportTools } from "./tools/report";
 
 // Define the Env type to match wrangler.json bindings
 type Env = { 
@@ -31,6 +32,7 @@ export class MyMCP extends McpAgent<Env, unknown, Props> {
     registerDateTool(this.server);
     setupAppointmentTools(this.server, this.env);
     registerEmailTools(this.server);
+    registerReportTools(this.server, this.env);
 
     // Initialize and start the calendar reminder service
     this.reminderService = new CalendarReminderService(this.env);
